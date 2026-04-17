7555db7c07a142e5e1bb84cc6ef7bbcc220335b5
Sivasakthi V <sivasakthiviswanathan07@gmail.com>
2026-04-17 15:58:16 +0530

    Added product data in products.json

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,19 +1,30 @@
 const ProductModel = require('../models/productModel');
 
+//GET PRODUCTS API - /api/v1/products
 exports.getProducts = async(req, res, next) =>{
-
     const products = await ProductModel.find({});
-
     res.json({
         success: true,
         products
     })
 }
 
-exports.getSingleProduct = (req, res, next) =>{
-    res.json({
+//GET SINGLEPRODUCT API - /api/v1/product/:id
+exports.getSingleProduct = async(req, res, next) =>{
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
         success: true,
-        message: 'Get Single Product Working!'
+        product
     })
+    } catch (error) {
+        res.status(404).json({
+        success: false,
+        message: 'Unable to GET product with that ID'
+    })
+    }
+    
+
+    
 }
 

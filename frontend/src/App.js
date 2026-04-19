5bafd8d1f310d055033a986fe9963fb922e91e49
Sivasakthi V <sivasakthiviswanathan07@gmail.com>
2026-04-19 17:40:59 +0530

    Created ProductDetails Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,21 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductDetail from './pages/ProductDetail';
+import { useState } from 'react';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <div className="App">
       <Router>
         <div>
-      <Header/>
+      <Header cartItems = {cartItems}/>
           <Routes>
           <Route path="/" element={<Home/>}  />
+          <Route path="/search" element={<Home/>}  />
+          <Route path="/product/:id" element={<ProductDetail/>}  />
           </Routes>
         </div>
       </Router>

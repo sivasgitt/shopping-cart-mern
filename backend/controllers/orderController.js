719fb96fcde05b71027406341889989d013db301
Sivasakthi V <sivasakthiviswanathan07@gmail.com>
2026-04-19 19:20:19 +0530

    Implemented cart functionality and REST APIs

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,11 +1,19 @@
 const orderModel = require('../models/orderModel');
+const productModel = require('../models/productModel');
+
 //Create Order - /api/v1/order
 exports.createOrder = async(req, res, next) => {
     const cartItems = req.body;
     const amount = Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)).toFixed(2);
     const status = 'pending';
-
     const order = await orderModel.create({cartItems, amount, status})
+
+    //UPDATING PRODUCT STOCK
+    cartItems.forEach(async (item) => {
+        const product = await productModel.findById(item.product._id);
+        product.stock -= item.qty;
+        await product.save();
+    })
 
     res.json(
         {
